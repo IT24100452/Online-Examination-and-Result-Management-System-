8c0c9bbdf7e09cf8d5a9d7ef5f437c4a9018932c
IT24100452 <it24100452@my.sliit.lk>
2025-05-06 21:07:04 +0530

    Add LogoutServlet

---
+++ src/main/java/OE_RM/servlets/LogoutServlet.java
@@ -0,0 +1,21 @@
+package OE_RM.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Invalidate the session
+        request.getSession().invalidate();
+        // Redirect to login page
+        response.sendRedirect("login");
+    }
+}
