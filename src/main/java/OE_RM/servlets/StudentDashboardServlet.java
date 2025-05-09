6084e77632e22276d75792a13d2695cb30ce7c03
IT24100842 <it24100842@my.sliit.lk>
2025-05-09 22:24:59 +0530

    Update by adding basic servlet and import statements

---
+++ src/main/java/OE_RM/servlets/StudentDashboardServlet.java
@@ -0,0 +1,8 @@
+package OE_RM.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
